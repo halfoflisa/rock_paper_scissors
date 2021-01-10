48ec1331e4a1b9801b61a798b7270df0605562ce
halfoflisa <thump3569@gmail.com>
2021-01-10 19:50:47 +0900

    add simple UI elements

---
+++ UIVer/readme.md
@@ -0,0 +1,5 @@
+Assignment:
+1. Go back to your “Rock Paper Scissors” game from a previous lesson and give it a simple UI so that the player can play the game by clicking on buttons rather than typing their answer in a prompt.
+2. Create three buttons, one for each selection. Add an event listener to the buttons that calls your playRound function with the correct playerSelection every time a button is clicked.
+3. Add a div for displaying results and change all of your console.logs into DOM methods.
+4. Display the running score, and announce a winner of the game once one player reaches 5 points.
---
+++ UIVer/script.js
@@ -0,0 +1,81 @@
+let playerScore = 0;
+let computerScore = 0;
+
+let btns = document.querySelectorAll('input');
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log(btn.getAttribute('value'));
+        if (playerScore < 5 && computerScore < 5){
+            playRound(btn);
+        }
+    });
+});
+
+function playRound(btn){
+        let computerSelection = Math.floor(Math.random()*3);
+        let playerSelection = btn.getAttribute('value');
+
+        function computerPlay(){
+                if(computerSelection === 0){
+                        computerSelection = "Rock";
+                        return computerSelection;
+                } else if(computerSelection === 1){
+                        computerSelection = "Paper";
+                        return computerSelection;
+                } else if(computerSelection === 2){
+                        computerSelection = "Scissors";
+                        return computerSelection;
+                }
+        }
+
+        computerPlay();
+
+        if(playerSelection === computerSelection) { 
+                let gameResult = document.getElementById('result');
+                gameResult.innerHTML = "The Result is a tie!";
+        } else if(playerSelection === "Rock"){
+                if(computerSelection === "Paper"){
+                        computerScore++;
+                        let gameResult = document.getElementById('result');
+                        gameResult.innerHTML = "You lose! Paper beats rock";
+                } else if(computerSelection === "Scissors"){
+                        playerScore++;
+                        let gameResult = document.getElementById('result');
+                        gameResult.innerHTML = "You win! Rock beats scissors"; 
+                }
+        } else if(playerSelection === "Paper"){
+                if(computerSelection === "Rock"){
+                        playerScore++;
+                        let gameResult = document.getElementById('result');
+                        gameResult.innerHTML = "You win! Paper beats rock";
+                } else if(computerSelection === "Scissors"){
+                        computerScore++;
+                        let gameResult = document.getElementById('result');
+                        gameResult.innerHTML = "You lose! Scissors beats paper";
+                }
+        } else if(playerSelection === "Scissors"){
+                if(computerSelection === "Paper"){
+                        playerScore++;
+                        let gameResult = document.getElementById('result');
+                        gameResult.innerHTML = "You win! Scissors beats paper";
+                } else if(computerSelection === "Rock"){
+                        computerScore++;
+                        let gameResult = document.getElementById('result');
+                        gameResult.innerHTML = "You lose! Rock beats scissors";
+                }
+        }
+
+        let playerPoint = document.getElementById('player');
+        playerPoint.innerHTML = playerScore;
+        let computerPoint = document.getElementById('computer');
+        computerPoint.innerHTML = computerScore;
+        
+        if (playerScore === 5){
+                let gameWinner = document.getElementById('winner');
+                gameWinner.innerHTML = "Congratulations! You Win!";
+        } else if (computerScore === 5){
+                let gameWinner = document.getElementById('winner');
+                gameWinner.innerHTML = "You Lose!";
+        }
+
+}
